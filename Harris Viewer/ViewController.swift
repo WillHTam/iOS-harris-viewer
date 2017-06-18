8cea47f35f512d6072cf84b33e355c071a7beba3
William T <william.tam18@gmail.com>
2017-06-18 18:14:31 +0800

    Initial Commit

---
+++ Harris Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Harris Viewer
+//
+//  Created by William on 18/6/17.
+//  Copyright © 2017 Will Works Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
