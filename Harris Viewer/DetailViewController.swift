3c75ca9039daa98b4cb621ceb727e450c8641f15
William T <william.tam18@gmail.com>
2017-06-18 23:17:50 +0800

    Images display now, aspect fit images

---
+++ Harris Viewer/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Harris Viewer
+//
+//  Created by William on 18/6/17.
+//  Copyright © 2017 Will Works Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    // this line created by ctrl+dragging from storyboard
+    // we need to use the image view inside code by creating a property that is attached to the layout
+    // hover over the grey circle to the left
+    // IBOutlet: there is a connection between this code and Interface Builder
+    // weak: don't own this object in memory, the view owns it
+    // imageView: the property name assigned to this UIIMageView
+    // UIImageView!: declares property to be of type UIImageView, and then implicitly unwrapped
+    
+    var selectedImage: String?
+    // hold the name of the image to load
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+        // if let unwraps the option in selectedImage
+        // if it has a value (which it always will and thus the ? will not fail), it will be placed in imageToLoad and then passed to UIImage and laoded
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Harris Viewer/ViewController.swift
+++ Harris Viewer/ViewController.swift
@@ -29,10 +29,37 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
-        
-        print(pictures)
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // instead of retyping argument, use underscore to automatically use the argument entered
+        // this function creates as many table rows as there are pictures
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // recycle cells into reuse queue
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        // set 'Picture' as the identifier of the cell type we want to reuse
+        cell.textLabel?.text = pictures[indexPath.row]
+        // will use the name of the picture from the array and put it into the cell's text label
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // instantiateViewController automatically returns UIViewController, so we have to typecast it
+            // 2: Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    // the 'if let' above has three checks 1) ? which would stop if storyboard were nil 2) instantiateViewController would fail if it called an invalid Identifier and 3) as? would fail if a different type of view controller were received
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
